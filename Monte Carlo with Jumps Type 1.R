857fa67b1564b39490c9a37d521abf39baca6f0d
alanchernoff <90869944+alanchernoff@users.noreply.github.com>
2022-08-17 20:10:13 -0400

    Update Monte Carlo with Jumps Type 1.R

--- Monte Carlo with Jumps Type 1.R
+++ Monte Carlo with Jumps Type 1.R
@@ -12,7 +12,7 @@ srho = sqrt(1-rho^2)
 
 #Simulation set up 
 
-n = 100 #number of simulations
+n = 100 #number of simulated days
 
 blanks=matrix(0,nrow=n,ncol=(t-1))
 y = cbind(matrix(m,nrow=n,ncol=1),blanks)
